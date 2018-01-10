ea91085e211876dadd93ecba54f815305cc46793
Zeb Pykosz <zeb.apy@gmail.com>
2018-01-10 10:52:33 -0500

    progress on framework

--- index.js
+++ index.js
@@ -1,1 +1,169 @@
 import './index.scss';
+(function() {
+  // set up background hovers to highlight related explainer item
+  var legend = document.querySelector('.legend');
+  var zones = document.querySelectorAll(
+    '.mission-bg, .directions-bg, .distinctions-bg, .principles-item'
+  );
+
+  function disableActiveLegendItem() {
+    var activeItem = document.querySelector('.legend-item.active');
+
+    if (activeItem) {
+      activeItem.classList.remove('active');
+    }
+  }
+
+  function handleZoneMouseOver(event) {
+    legend.classList.add('active');
+
+    disableActiveLegendItem();
+
+    var elem = event.target;
+
+    var zone = elem.getAttribute('data-zone');
+
+    var target = document.querySelector('#legend-item-' + zone);
+
+    target.classList.add('active');
+  }
+
+  function handleZoneMouseOut(event) {
+    legend.classList.remove('active');
+
+    disableActiveLegendItem();
+  }
+
+  [].forEach.call(zones, function(elem) {
+    elem.addEventListener('mouseover', handleZoneMouseOver);
+    elem.addEventListener('mouseout', handleZoneMouseOut);
+  });
+
+  var directionsZone = document.querySelector('.directions-bg');
+  var distinctionZone = document.querySelector('.distinctions-bg');
+
+  // set up item modals
+  var items = document.querySelectorAll('.js-framework-item');
+
+  var modal = document.querySelector('.framework .modal');
+  var modalTag = modal.querySelector('.modal-tag');
+  var modalTitle = modal.querySelector('.modal-title');
+  var modalText = modal.querySelector('.modal-text');
+  var modalCloseButton = modal.querySelector('.modal-close');
+  var modalPrevButton = modal.querySelector('.modal-prev');
+  var modalNextButton = modal.querySelector('.modal-next');
+
+  function setModalContent(tag, title, text, modifier) {
+    modalTag.innerText = tag;
+    modalTitle.innerText = title;
+    modalText.innerText = text;
+    if (tag) {
+      modal.classList.add('modal-' + tag);
+    }
+  }
+
+  function openModal(elem) {
+    var values = getItemValues(elem);
+
+    setModalContent(values.tag, values.title, values.text);
+
+    elem.classList.add('active');
+    modal.classList.add('active');
+  }
+
+  function getActiveItem() {
+    return document.querySelector('.js-framework-item.active');
+  }
+
+  function closeModal() {
+    modal.classList.remove('active');
+    modal.classList.remove('modal-direction', 'modal-distinction');
+
+    var activeItem = getActiveItem();
+    if (activeItem) {
+      activeItem.classList.remove('active');
+    }
+  }
+
+  function getItemValues(elem) {
+    var titleElem = elem.closest('[data-title]');
+
+    var tag = titleElem.getAttribute('data-title');
+    var title = elem.innerText;
+    var text = elem.getAttribute('data-text');
+
+    return {
+      tag: tag,
+      title: title,
+      text: text
+    };
+  }
+
+  function handleItemClick(e) {
+    var elem = e.target;
+    openModal(elem);
+  }
+
+  // bind clickable items
+  [].forEach.call(items, function(item) {
+    item.addEventListener('click', handleItemClick);
+  });
+
+  function handleNextClick(event) {
+    var activeItem = getActiveItem();
+
+    if (!activeItem) {
+      return;
+    }
+
+    closeModal();
+
+    var parent = activeItem.parentElement;
+    var nextElem = parent.nextElementSibling;
+
+    if (!nextElem) {
+      nextElem = parent.closest('[data-title]').firstElementChild;
+    }
+
+    var nextLink = nextElem.querySelector('a');
+
+    if (!nextLink) {
+      return;
+    }
+
+    openModal(nextLink);
+  }
+
+  function handlePrevClick(event) {
+    var activeItem = getActiveItem();
+
+    if (!activeItem) {
+      return;
+    }
+
+    closeModal();
+
+    var parent = activeItem.parentElement;
+    var prevElem = parent.previousElementSibling;
+
+    if (!prevElem) {
+      prevElem = parent.closest('[data-title]').lastElementChild;
+    }
+
+    var prevLink = prevElem.querySelector('a');
+
+    if (!prevLink) {
+      return;
+    }
+
+    openModal(prevLink);
+  }
+
+  function handleCloseClick(event) {
+    closeModal();
+  }
+
+  modalNextButton.addEventListener('click', handleNextClick);
+  modalPrevButton.addEventListener('click', handlePrevClick);
+  modalCloseButton.addEventListener('click', handleCloseClick);
+})();
